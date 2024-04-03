56d29b677e0345f7add29a4122b3dd6b5fd7dd2b
amillj <amilljaljak@hotmail.com>
2024-04-03 15:43:19 +0200

    Project structure for VitalFlow
    Implemented base functionalities

---
+++ VitalFlow/VitalFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VitalFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
