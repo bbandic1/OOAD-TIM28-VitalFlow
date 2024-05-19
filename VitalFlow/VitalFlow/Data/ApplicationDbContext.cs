d98fb284314403bc280cb517696c0e57e4639fc6
bbandic1 <bbandic1@etf.unsa.ba>
2024-05-19 04:52:12 +0200

    Završen DBContext

--- VitalFlow/VitalFlow/Data/ApplicationDbContext.cs
+++ VitalFlow/VitalFlow/Data/ApplicationDbContext.cs
@@ -18,9 +18,13 @@ namespace VitalFlow.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Student>().ToTable("Student");
-            modelBuilder.Entity<UpisNaPredmet>().ToTable("UpisNaPredmet");
-            modelBuilder.Entity<Predmet>().ToTable("Predmet");
+            modelBuilder.Entity<Korisnik>().ToTable("Korisnik");
+            modelBuilder.Entity<Zahtjev>().ToTable("Zahtjev");
+            modelBuilder.Entity<Termin>().ToTable("Termin");
+            modelBuilder.Entity<Zahtjev_Hub>().ToTable("Zahtjev_Hub");
+            modelBuilder.Entity<Termin_Hub>().ToTable("Termin_Hub");
+            modelBuilder.Entity<Zaliha>().ToTable("Zaliha");
+            modelBuilder.Entity<Hub>().ToTable("Hub");
             base.OnModelCreating(modelBuilder);
         }
     }
