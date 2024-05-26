1f204365da2692f4c752ee606a31684833aea905
bbandic1 <bbandic1@etf.unsa.ba>
2024-05-26 11:12:04 +0200

    ZahtjevController generisan

--- VitalFlow/VitalFlow/obj/Debug/net8.0/VitalFlow.AssemblyInfo.cs
+++ VitalFlow/VitalFlow/obj/Debug/net8.0/VitalFlow.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitalFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f520c261f9d52d5eae35b25840d97e08a3c75b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2073620df5dbc65825ec19fc14ec9c574ddaaa7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitalFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitalFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
